fe92cc3b4ef076431ce9cbc1b1326fab70375e37
Owen ANCELOT <owen.ancelot@la-projets.com>
2023-11-09 19:47:42 +0100

    Run with 70000 numbers  ✨

--- classifier.py
+++ classifier.py
@@ -1,6 +1,14 @@
 from neuron import Neuron
 
 
+def argmax(values):
+    index = 0
+    for i in range(len(values)):
+        if values[i] > values[index]:
+            index = i
+    return index
+
+
 class Classifier:
     def __init__(self, inputs, classes, learning_rate):
         self.neurons = []
@@ -17,5 +25,4 @@ class Classifier:
         outputs = []
         for n in self.neurons:
             outputs.append(n.run(inputs))
-
-        return outputs
+        return argmax(outputs)
--- neuron-network.py
+++ neuron-network.py
@@ -1,9 +1,18 @@
+import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix
 from classifier import Classifier
+from os.path import exists
+
+FILE_MODEL = "model.dat"
+LEARNING_RATE = 1E-3
+NB_ITERATION = 1
+DATASET_NAME = "dataset-full.csv"
+
 
 def get_data():
-    data = np.loadtxt('dataset.csv', skiprows=1, delimiter=',')
+    data = np.loadtxt(DATASET_NAME, skiprows=1, delimiter=',')
     inputs = data[:, :- 1] / 255
     outputs = data[:, -1]
     return inputs, outputs
@@ -13,20 +22,28 @@ def one_hot_encoder(value, values):
     return [1 if x == value else 0 for x in values]
 
 
-if __name__ == '__main__':
-    inputs, outputs = get_data()
-    classifier = Classifier(len(inputs[0]), 10, 1E-3)
+def learning(classifier, inputs, outputs):
     errors = []
-    for j in range(300):
+    for j in range(NB_ITERATION):
         for i in range(len(inputs)):
             desired = one_hot_encoder(outputs[i], list(range(10)))
-            errors.append(classifier.learn(inputs[i],desired))
+            errors.append(classifier.learn(inputs[i], desired))
     plt.plot(errors)
     plt.show()
 
-    error = 0.0
+
+if __name__ == '__main__':
+    inputs, outputs = get_data()
+    if not exists(FILE_MODEL):
+        classifier = Classifier(len(inputs[0]), 10, LEARNING_RATE)
+        learning(classifier, inputs, outputs)
+        with open(FILE_MODEL, 'wb') as file:
+            pickle.dump(classifier, file)
+    else:
+        with open(FILE_MODEL, 'rb') as file:
+            classifier = pickle.load(file)
+
     predictions = []
     for i in range(len(inputs)):
-        prediction = classifier.run(inputs[i])
-        print(prediction, ' -- ', outputs[i])
-
+        predictions.append(classifier.run(inputs[i]))
+    print(confusion_matrix(outputs, predictions))
